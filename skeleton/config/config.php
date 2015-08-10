bab320039a589cd5d1e02cd6980a9c9e10ab19bf
Joshua Williams <com.joshua360@gmail.com>
2015-08-09 21:42:29 -0400

    skeleton update

---
+++ skeleton/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+return array(
+	'debug' => false,
+	'path' => rtrim(dirname(getcwd())),
+	'application' => Vars::getFrom($_SERVER, 'HTTP_HOST', 'Application'),
+	'site_url' => false,
+	'site_url_ssl' => false,
+	'sesson_timeout' => 30,
+	'form_timeout' => 10,
+	'enc_key' => 'my_encryption_key',
+	'templateEngine' => false,
+	'segmentOffset' => 0,
+	'modules' => false,
+	'defaultModule' => false,
+);
+?>
