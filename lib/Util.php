3da4eaa1ee41fdaffa17dbb583cac9c941ec805e
Joshua Williams <com.joshua360@gmail.com>
2015-08-10 01:49:41 -0400

    Util class update. encypt, decrypt, generateKey methods added

--- lib/Util.php
+++ lib/Util.php
@@ -27,6 +27,33 @@ namespace JFrame{
 			}
 			return $var;
 		}
+		
+		public static function encrypt($key, $plaintext){
+			$td = mcrypt_module_open(MCRYPT_RIJNDAEL_256, '', MCRYPT_MODE_CBC, '');
+			$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+			mcrypt_generic_init($td, $key, $iv);
+			$crypttext = mcrypt_generic($td, $plaintext);
+			mcrypt_generic_deinit($td);
+			return base64_encode($iv.$crypttext);
+		}
+		
+		public static function decrypt($key, $crypttext){
+			$crypttext = base64_decode($crypttext);
+			$plaintext = '';
+			$td        = mcrypt_module_open(MCRYPT_RIJNDAEL_256, '', MCRYPT_MODE_CBC, '');
+			$ivsize    = mcrypt_enc_get_iv_size($td);
+			$iv        = substr($crypttext, 0, $ivsize);
+			$crypttext = substr($crypttext, $ivsize);
+			if ($iv) {
+				mcrypt_generic_init($td, $key, $iv);
+				$plaintext = mdecrypt_generic($td, $crypttext);
+			}
+			return trim($plaintext);
+		}
+		
+		public static function generateKey($max_length=20){
+			return ($random = substr(md5(rand()),0,$max_length));
+		}
 	}
 }
 
