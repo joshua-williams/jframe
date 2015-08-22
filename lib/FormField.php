cb162c7c9f2c9975e04b47f4059068022c4352de
Joshua Williams <com.joshua360@gmail.com>
2015-08-21 20:02:09 -0400

    seperated FormField from Form and added FormField::removeAttr property

---
+++ lib/FormField.php
@@ -0,0 +1,126 @@
+<?php 
+
+namespace JFrame{
+	
+	/**
+	 * @desc Form Field Class
+	 */
+	class FormField{
+		private $type;
+		private $label;
+		private $value;
+		private $options;
+		private $attributes = array();
+	
+		function __construct(Array $prop){
+			$this->type = Vars::getFrom($prop, 'type');
+			$this->label = Vars::getFrom($prop, 'label');
+			$this->value = Vars::getFrom($prop, 'value');
+			$this->options = Vars::getFrom($prop, 'options');
+			foreach($prop as $attr=>$val){
+				if($attr === 'type') continue;
+				$this->attr($attr, $val);
+			}
+			//$attr = Vars::getFrom($prop, 'attr', Vars::getFrom($prop, 'attributes'));
+		}
+		public function type(){
+			return $this->type;
+		}
+	
+		public function label(){
+			return $this->label;
+		}
+	
+		public function value(){
+			return $this->attr('value');
+		}
+	
+		public function attr($attr=null, $value='no_val'){
+			if(!$attr) return $this->attributes;
+			if(is_string($attr)){
+				if($value === 'no_val'){
+					return Vars::getFrom($this->attributes, $attr);
+				}
+				if(!is_string($value)) return false;
+				$this->attributes[$attr] = $value;
+			}elseif(is_array($attr)){
+				foreach($attr as $_attr=>$_value){
+					$this->attr($_attr, $_value);
+				}
+			}
+		}
+		
+		public function removeAttr($attr){
+			if(!isset($this->attributes[$attr])) return false;
+			unset($this->attributes[$attr]);
+		}
+		
+		public function render(){
+			switch($this->type){
+				case 'text':
+				case 'password':
+				case 'hidden':
+				case 'file':
+				case 'submit':
+				case 'radio': return $this->renderText(); break;
+				case 'textarea': return $this->renderTextArea(); break;
+				case 'dropdown': return $this->renderDropdown(); break;
+			}
+		}
+	
+		function isValid(){
+			$fieldTypes = array('text','email','password','number','textarea','radio','checkbox','dropdown', 'file','hidden','submit');
+			if(!in_array($this->type, $fieldTypes)) return false;
+			if($this->type == 'dropdown'){
+				if(!$this->options || !is_array($this->options)) return false;
+				$options = array();
+				foreach($this->options as $option){
+					if(!isset($option['value']) && !isset($option['label'])) continue;
+					$o['value'] = Vars::getFrom($option,'value', $option['label']);
+					$o['label'] = Vars::getFrom($option, 'label', $option['value']);
+					$o['selected'] = Vars::getFrom($option, 'selected');
+					$options[] = $o;
+				}
+				if(!$options) return false;
+				$this->options = $options;
+			}
+			return true;
+		}
+	
+		private function renderAttributes(){
+			$attributes = array();
+			foreach($this->attributes as $key=>$val){
+				$attributes[] = $key . '="' . str_replace('"','\\"', $val) . '"';
+			}
+			return implode(' ', $attributes);
+		}
+	
+		private function renderLabel(){
+			return "<label>$this->label</label>";
+		}
+	
+		private function renderValue(){
+			return ($this->value) ? ' '.$this->value : '';
+		}
+	
+		private function renderText(){
+			return "<input type='$this->type' " . $this->renderAttributes() . $this->renderValue() . "/>";
+				
+		}
+	
+		private function renderTextarea(){
+			return "<textarea " . $this->renderAttributes() . ">" . $this->renderValue() . "</textarea>";
+		}
+	
+		private function renderDropdown(){
+			$html = "<select " . $this->renderAttributes() . ">";
+			foreach($this->options as $option){
+				$html.= "<option value='" . $option['value'] . "'>" . $option['label'] . "</option>";
+			}
+			$html.="</select>";
+			return $html;
+		}
+	}
+}
+
+?>
